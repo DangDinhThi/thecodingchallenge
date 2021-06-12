f6476dac06ed0a02dd93591ae3fba1ca3de0aa47
songhuy304 <85212659+songhuy304@users.noreply.github.com>
2021-06-12 21:16:35 +0700

    Update day_2.cpp

--- day_2.cpp
+++ day_2.cpp
@@ -24,7 +24,7 @@ void solve(double meal_cost, int tip_percent, int tax_percent) {
     //tinh total
 	double tip=(meal_cost * tip_percent)/100;
     double tax=(meal_cost * tax_percent)/100;
-    int total_cost=meal_cost + tip + tax;
+    int total_cost=(int)round(meal_cost+tip+tax);
    	//in ra kq
     printf("%d", total_cost);
 }
