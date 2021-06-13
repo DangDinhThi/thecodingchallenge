1ef6eb09fbcb70a1d85e3605494b91da71351ec5
Dang Toan <toanc3k55@gmail.com>
2021-06-13 10:08:07 +0700

    Toan: update day_3

--- day_3.cpp
+++ day_3.cpp
@@ -1,17 +1,20 @@
-    #include<stdio.h>
-    int main(){
+#include<stdio.h>
+int main(){
 	int n;
-	if(n % 2==0){
-        if(n>=2 && n<=5)
-        {
-        printf("Not Weird");
-        }else if(n>=6 && n<=20){
-            printf("Weird");
-        }else{
-            printf("Not Weird");
-        }
-    }
-    else{
-        printf("Weird");
-    }
+	scanf("%d", &n);
+	if(n%2!=0){
+		printf("Werird");
+	}
+	if(n%2==0 && n>=2 && n<=5){
+		printf("Not Wreird");
+		
+	}
+	if(n%2==0 && n>=6 && n<=20){
+		printf("Wreird");
+		
+	}
+	if(n%2==0 && n>20){
+		printf("Not Wreird");
+		
+	}
 }
